92089c59b1470e772e57753115c6050af060bf07
fengfancky <fengfancky@gmail.com>
2019-06-18 15:54:30 +0800

    server

--- exam_socketServer.js
+++ exam_socketServer.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 httpd.listen(4001);
 
 var keyNameId = {};
+var socketArray = new Array();
 
 function handler(req,res){
 
@@ -16,42 +17,25 @@ function handler(req,res){
    
 }
 
-function getValue(obj,to){
-    for(var key in obj){
-        if(obj[key] == to){
-            return key;
-        }
-    }
-}
-
 
 io.sockets.on('connection',function(socket){
 
     socket.on('clientMessage',function(content){
         // socket.emit('serverMessage',content);
-        // socket.broadcast.emit('serverMessage',content);
-
-        io.sockets.socket(getValue(keyNameId,"TV")).emit('serverMessage',content);
+        socket.broadcast.emit('serverMessage',content);
     });
 
 
     socket.on('login',function(username){
-        
-        if(username == null){
-            keyNameId[socket.id] = socket.id;
-        }else{
-            keyNameId[socket.id] = username;
-        }
-        
-        // socket.emit('serverTipMessage','你已加入聊天');
-        // socket.broadcast.emit('serverTipMessage', username +' 加入聊天');
+        socket.emit('serverTipMessage','你已加入聊天');
+        socket.broadcast.emit('serverTipMessage', username +' 加入聊天');
         
     });
 
-    // socket.on('disconnect',function(){
-    //     socket.emit('serverTipMessage','你已退出聊天');
-    //     socket.broadcast.emit('serverTipMessage', keyNameId[socket.id]+' 退出聊天');
-    // });
+    socket.on('disconnect',function(){
+        socket.emit('serverTipMessage','你已退出聊天');
+        socket.broadcast.emit('serverTipMessage', keyNameId[socket.id]+' 退出聊天');
+    });
 
     socket.emit('login');
 })
