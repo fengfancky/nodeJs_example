ede1b1ecbe202cec2eefdcd2b1dd6cc55e75a69f
fengfancky <fengfancky@gmail.com>
2019-06-18 11:19:54 +0800

    client

--- exam_socketServer.js
+++ exam_socketServer.js
@@ -19,18 +19,18 @@ function handler(req,res){
 io.sockets.on('connection',function(socket){
 
     socket.on('clientMessage',function(content){
-        socket.emit('serverMessage',content);
-        socket.broadcast.emit('serverMessage',content);
+        socket.emit('serverMessage',{value:content,current:true,name:keyNameId[socket.id]});
+        socket.broadcast.emit('serverMessage',{value:content,current:false,name:keyNameId[socket.id]});
     });
 
 
     socket.on('login',function(username){
         
-        // if(username == null){
-        //     keyNameId[socket.id] = socket.id;
-        // }else{
-        //     keyNameId[socket.id] = username;
-        // }
+        if(username == null){
+            keyNameId[socket.id] = socket.id;
+        }else{
+            keyNameId[socket.id] = username;
+        }
         
         socket.emit('serverTipMessage','你已加入聊天');
         socket.broadcast.emit('serverTipMessage', username +' 加入聊天');
